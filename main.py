8627b682f05154ca4bda2fd22f7d65f6d7beb6c2
ragav1903 <109259761+ragav1903@users.noreply.github.com>
2024-04-11 21:04:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import streamlit as st
+from PIL import Image
 import instaloader
 from nltk.sentiment import SentimentIntensityAnalyzer
 import nltk
 nltk.download('vader_lexicon')
 
 sia = SentimentIntensityAnalyzer()
-loader = instaloader.Instaloader()
 
 def analyze_sentiment(comment):
     sentiment_scores = sia.polarity_scores(comment)
@@ -57,44 +57,38 @@ def display_sentiment_summary(commentslst):
 comments = []
 
 def HomePage():
-    comments.clear()  # Clear the comments list
-    st.title("Get the sentimental analysis of any profile you want")
-    insta_id = st.text_input("Enter the Instagram ID: ")
-    if st.button("Get"):
-        try:
-            profile = instaloader.Profile.from_username(loader.context, insta_id)
-            for post in profile.get_posts():
-                post_comments = post.get_comments()
-                for comment in post_comments:
-                    comments.append(comment.text)
-            st.title("Comments")
-            st.text_area(label='', value='\n'.join(comments), height=300)
-            display_sentiment_chart(comments)
-            display_sentiment_summary(comments)
-        except instaloader.exceptions.LoginRequiredException:
-            st.error("Login required. Please provide valid Instagram login credentials.")
-        except Exception as e:
-            st.error(f"An error occurred: {str(e)}")
+  comments.clear()  # Clear the comments list
+  st.title("Get the sentimental analysis of any profile you want")
+  insta_id = st.text_input("Enter the Instagram ID: ")
+  if st.button("Get"):
+    profile = instaloader.Profile.from_username(loader.context, insta_id)
+    for post in profile.get_posts():
+      post_comments = post.get_comments()
+      for comment in post_comments:
+          comments.append(comment.text)
+    st.title("Comments")
+    st.text_area(label='', value='\n'.join(comments), height=300)
+    display_sentiment_chart(comments)
+    display_sentiment_summary(comments)
 
 def main_page():
-    st.title("Enter the login details of your Instagram ID")
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-    if st.button("Login"):
-        try:
-            st.write("logging in")
-            loader.login(username, password)
-            st.success("Logged in as {}".format(username))
-            st.write("Redirecting to home page...")
-            st.session_state.runpage = HomePage
-            st.experimental_rerun()
-        except instaloader.exceptions.InvalidArgumentException:
-            st.error("Invalid username or password")
-        except instaloader.exceptions.ConnectionException:
-            st.error("Connection error. Please check your internet connection.")
-        except Exception as e:
-            st.error(f"An error occurred: {str(e)}")
+  global username, password, loader
+  st.title("Enter the login details of your Instagram ID")
+  username = st.text_input("Username")
+  password = st.text_input("Password", type="password")
+  if st.button("Login"):
+    try:
+          loader = instaloader.Instaloader()
+          st.write("logging in")
+          loader.login(username, password)
+          st.success("Logged in as {}".format(username))
+          st.write("Redirecting to home page...")
+          st.session_state.runpage = HomePage
+          st.session_state.runpage
+          st.experimental_rerun()
+    finally:
+          st.error("Invalid username or password")
 
 if "runpage" not in st.session_state:
-    st.session_state.runpage = main_page
+  st.session_state.runpage = main_page
 st.session_state.runpage()
